35697c87bba373bba0bef0a12f0cf3ec63f83bc6
cascadiabooking <Anton@Antons-MacBook-Pro.local>
2016-07-30 23:15:51 -0700

    js

--- js/contact_me_static.js
+++ js/contact_me_static.js
@@ -21,3 +21,34 @@ $(function() {
 $('#name').focus(function() {
     $('#success').html('');
 });
+
+var $contactForm = $("#contact-form"),
+	$submitButton = $contactForm.find('[type="submit"]');
+
+$.ajax({
+	url: getFormSubmitURL(),
+	method: "POST",
+	data: $contactForm.serialize(),
+	dataType: "json",
+	beforeSend: function(){
+		/* Disable the submit button to prevent multiple submissions
+		Tell the user that the form is sending */
+		$submitButton.attr("disabled", true);
+		console.log("Sending message...");
+	},
+	success: function(data){
+		/* Enable the submit button
+		Tell the user that their message was sent
+		Clear all the form fields */
+		$submitButton.attr("disabled", false);
+		console.log("Message sent.");
+		$contactForm.trigger("reset");
+	},
+	error: function(err){
+		/* Enable the submit button
+		Tell the user that the message failed to send */
+		$submitButton.attr("disabled", false);
+		console.log("Something went wrong. Try again.");
+	}
+});
+
