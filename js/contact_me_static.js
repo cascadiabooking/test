1a47e9c0e54585456fa7c7a9cf207c958f562d1e
cascadiabooking <Anton@Antons-MacBook-Pro.local>
2016-07-31 00:41:42 -0700

    back

--- js/contact_me_static.js
+++ js/contact_me_static.js
@@ -1,37 +1,3 @@
-function getFormSubmitURL(){
-	var base64_email = "cmFuc2FuMzJAeWFob28uY29t";
-	return "//formspree.io/" + atob(base64_email);
-}
-
-function showAlert($container, $alert){
-	$container.empty().append($alert);
-}
-
-function submitContactForm($contactForm){
-	var $alertContainer = $(".row-alert-container"),
-			$submitButton = $contactForm.find('[type="submit"]');
-
-	$.ajax({
-		url: getFormSubmitURL(),
-		method: "POST",
-		data: $contactForm.serialize(),
-		dataType: "json",
-		beforeSend: function(){
-			$submitButton.attr("disabled", true);
-			showAlert($alertContainer, '<div class="alert"><i class="fa fa-spin fa-spinner"></i>Sending message...</div>');
-		},
-		success: function(data){
-			$submitButton.attr("disabled", false);
-			showAlert($alertContainer, '<div class="alert success"><i class="fa fa-check-circle"></i>Message sent.</div>');
-			$contactForm.trigger("reset");
-		},
-		error: function(err){
-			$submitButton.attr("disabled", false);
-			showAlert($alertContainer, '<div class="alert danger"><i class="fa fa-times-circle"></i>Something went wrong. Try again.</div>');
-		}
-	});
-};
-=======
 $(function() {
 
     $("input,textarea").jqBootstrapValidation({
@@ -55,7 +21,3 @@ $(function() {
 $('#name').focus(function() {
     $('#success').html('');
 });
-
-
-
->>>>>>> origin/master
